879d9ec201c41355c30dbe1030d5b642aa406c67
Nils <nilsscharein@yahoo.de>
2026-06-21 14:24:30 +0200

    project INIT (add example Code and MUI)

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# PKTrace
-A spotmap for traceurs with extra features like tutorials, challenges and earning reallife badges.
+# README
---
+++ src/react/location/GeoForm.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+
+const GeoInfo = ({ lat, long}) => (
+  <div
+    style={{
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center'
+    }}
+  >
+    <div style={{ marginBottom: '5px' }}>
+      <span
+        style={{
+          display: 'inline-block',
+          width: '50px'
+        }}
+      >
+        Lat:
+      </span>
+      <input
+        type="text"
+        value={lat}
+      />
+    </div>
+    <div>
+      <span
+        style={{
+          display: 'inline-block',
+          width: '50px'
+        }}
+      >
+        Long:
+      </span>
+      <input
+        type="text"
+        value={long}
+      />
+    </div>
+  </div>
+)
+
+export default GeoInfo
