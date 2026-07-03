940cdb851de56ede67582cdb087a2c971f7b1152
Ryu Siegert <ryu.siegert@gmail.com>
2026-07-03 14:20:37 +0200

    Added Challenges and their appearance

--- src/react/Components/challenges/Challenges.jsx
+++ src/react/Components/challenges/Challenges.jsx
@@ -1,14 +1,28 @@
 import { Description } from '@mui/icons-material';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
 import { title } from 'process';
-import React from 'react'
+import { challenges, difficultyColors } from "../../../data/challenge"
+import "./Challenge.css"
+
+import React, { useState } from 'react'
 
 const Challenges = () => <h1>Challenges for you XD</h1>
 
 const AllChallenges = () => {
+    const [clicked, onClickChange] = useState(null);
+    return(
+        <>
+        <h1>All Challenges</h1>
+        <Stack spacing={2} direction="column">
+            {challenges.map((challenge) => (
+                <Button style={{backgroundColor: difficultyColors[challenge.level]}} variant="contained">{challenge.title}</Button>
+            ))}
+        </Stack>
+        </>
+    );
+};
 
-}
 
 
-
-
-export default AllChallenges
+export default AllChallenges;
